bae31e6a6b84455173705c7a767ff95c065f324e
Amit Singh <amit.singh@glideinvest.com>
2022-02-08 22:22:38 +0530

    Added context

--- App.js
+++ App.js
@@ -1,8 +1,41 @@
-import React from 'react';
+import React, {useReducer} from 'react';
 import Navigator from './src/navigation';
+import BlogContext from './src/AppContext';
+
+/**
+ * blog={id,title,content}
+ *
+ */
 
 const App = () => {
-  return <Navigator />;
+  const [data, dispatchData] = useReducer(blogReducer, []);
+
+  function blogReducer(blogs, action) {
+    switch (action.type) {
+      case 'CREATE_BLOG':
+        return [...blogs, action.payload.data];
+      case 'DELETE_BLOG':
+        return blogs.filter(blog => blog.id !== action.payload.id);
+      case 'EDIT_BLOG':
+        const toBeEditedBlogIndex = blogs.findIndex(
+          blogItem => blogItem.id === action.payload.data.id,
+        );
+        if (toBeEditedBlogIndex !== -1) {
+          let newBlogArr = [...blogs];
+          newBlogArr[toBeEditedBlogIndex] = action.payload.data;
+          return newBlogArr;
+        }
+        return blogs;
+      default:
+        return blogs;
+    }
+  }
+
+  return (
+    <BlogContext.Provider value={{data, dispatchData}}>
+      <Navigator />
+    </BlogContext.Provider>
+  );
 };
 
 export default App;
--- src/screens/BlogHome/index.js
+++ src/screens/BlogHome/index.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, {useContext, useEffect} from 'react';
 import {Text} from 'react-native';
 
+import BlogContext from '../../AppContext';
+
 const BlogHome = () => {
+  const blogContext = useContext(BlogContext);
+  console.log('rendered');
+  useEffect(() => {
+    const action = {
+      type: 'CREATE_BLOG',
+      payload: {
+        data: {
+          id: 0,
+          title: 'My Success',
+          content:
+            'These are my marching steps towards my success, which is surreal!',
+        },
+      },
+    };
+    blogContext.dispatchData(action);
+  }, []);
   return <Text>Blog Home</Text>;
 };
 
